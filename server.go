763c22e03f7893125acceb09f95167093047d21b
De_Lucio <kvartyra125@gmail.com>
2025-02-26 11:07:05 +0300

    Server fixed. Serve online now

--- server.go
+++ server.go
@@ -7,18 +7,30 @@ import (
 )
 
 
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("Pages/main.html")
+	if err != nil {
+		http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+
 func handleRequest() {
 	http.Handle("/Styles/", http.StripPrefix("/Styles/", http.FileServer(http.Dir("./Styles/"))))
 	http.Handle("/Pages/", http.StripPrefix("/Pages/", http.FileServer(http.Dir("./Pages/"))))
 	http.HandleFunc("/", index)
 
 	err := http.ListenAndServe(":8080", nil)
-
+	if err != nil {
+		fmt.Println("connect error")
+	}
 }
 
 func main() {
 
-
+fmt.Println("start")
 
 
 	handleRequest()
