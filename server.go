8e3a7589d7f62205ab60a33284d29e68259d9e82
De_Lucio <kvartyra125@gmail.com>
2025-02-26 10:57:27 +0300

    added early version of server + added main page

---
+++ server.go
@@ -0,0 +1,25 @@
+﻿package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+
+func handleRequest() {
+	http.Handle("/Styles/", http.StripPrefix("/Styles/", http.FileServer(http.Dir("./Styles/"))))
+	http.Handle("/Pages/", http.StripPrefix("/Pages/", http.FileServer(http.Dir("./Pages/"))))
+	http.HandleFunc("/", index)
+
+	err := http.ListenAndServe(":8080", nil)
+
+}
+
+func main() {
+
+
+
+
+	handleRequest()
+}
