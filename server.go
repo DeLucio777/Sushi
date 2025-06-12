6f1b2080018248537118df6947b5de73e00b04ba
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2025-06-12 19:47:32 +0300

    add user and items handle

--- server.go
+++ server.go
@@ -2,6 +2,7 @@
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/lib/pq"
 	"html/template"
@@ -19,6 +20,111 @@ type Item struct {
 	Description string  `json:"description"`
 	Name        string  `json:"item_name"`
 	Cost        float64 `json:"cost"`
+	ItemType    string  `json:"item_type"`
+}
+
+type UsersToItems struct {
+	Id     int    `json:"id"`
+	UserId int    `json:"user_id"`
+	Items  []Item `json:"items"`
+}
+
+func userHandel(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getUserHandel(w, r)
+
+	case http.MethodPost:
+		setUserHandel(w, r)
+	default:
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+	}
+}
+
+func itemsHandel(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getItemsHandel(w, r)
+	default:
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+	}
+}
+
+func userToItemsHandel(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getItemsHandel(w http.ResponseWriter, r *http.Request) {
+	db, err := connectDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+	rows, err := db.Query("SELECT * FROM tbl_item")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+	items := []Item{}
+	for rows.Next() {
+		var item Item
+		err := rows.Scan(&item.ID, &item.Image, &item.Description, &item.Name, &item.Cost, &item.ItemType)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		items = append(items, item)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(items)
+
+}
+
+func getUserHandel(w http.ResponseWriter, r *http.Request) {
+	db, err := connectDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+	name := r.URL.Query().Get("name")
+	password := r.URL.Query().Get("password")
+
+	var id int
+	err = db.QueryRow("SELECT ID FROM tbl_users WHERE name = $1 AND password = $2", name, password).Scan(&id)
+	exists := id != 0
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]bool{"exists": exists})
+}
+
+func setUserHandel(w http.ResponseWriter, r *http.Request) {
+	db, err := connectDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+
+	}
+	defer db.Close()
+	name := r.URL.Query().Get("name")
+	password := r.URL.Query().Get("password")
+	var id int
+	err = db.QueryRow("SELECT ID FROM tbl_users WHERE name = $1 AND password = $2", name, password).Scan(&id)
+	exists := id != 0
+	if exists {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = db.Query(`INSERT INTO tbl_users (name, password) VALUES ($1, $2)`, name, password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(nil)
 }
 
 func connectDB() (*sql.DB, error) {
@@ -49,7 +155,9 @@ func handleRequest() {
 	http.Handle("/Styles/", http.StripPrefix("/Styles/", http.FileServer(http.Dir("./Styles/"))))
 	http.Handle("/Pages/", http.StripPrefix("/Pages/", http.FileServer(http.Dir("./Pages/"))))
 	http.HandleFunc("/", index)
-
+	http.HandleFunc("/api/items", itemsHandel)
+	http.HandleFunc("/api/users", userHandel)
+	http.HandleFunc("/api/items_to_users", itemsHandel)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("connect error")
