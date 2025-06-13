e8b6f13874c076c840ca998064a6e6941d2b4509
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2025-06-13 09:07:43 +0300

    finish backend of project

--- DataBase/seeds.sql
+++ DataBase/seeds.sql
@@ -7,8 +7,9 @@ VALUES ('image1.jpg', 'A beautiful item 1', 'Item 1', 19.99,1),
        ('image2.jpg', 'A useful item 2', 'Item 2', 29.99,2),
        ('image3.jpg', 'An amazing item 3', 'Item 3', 39.99,3);
 INSERT INTO tbl_users_to_items (user_id, item_id)
-VALUES (1, 1), -- Alice owns Item 1
-       (1, 2), -- Alice owns Item 2
-       (2, 3), -- Bob owns Item 3
-       (3, 1); -- Charlie owns Item 1
-select * from tbl_users
+VALUES (1, 1), 
+       (1, 2), 
+       (2, 3), 
+       (3, 1); 
+
+
